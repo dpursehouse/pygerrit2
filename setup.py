37b8b76c49b3f0a5f8a26434f9653b0d3327c7a2
David Pursehouse <david.pursehouse@sonymobile.com>
2012-07-31 15:18:31 +0900

    Add setup.py
    Add setup.py which is needed to install pygerrit as a python package.
    
    Also added build-generated files to .gitignore.
    
    Change-Id: I0883debc370d679d0d872cdaf136399c0e0bcf43

---
+++ setup.py
@@ -0,0 +1,16 @@
+""" Client library for interacting with Gerrit. """
+
+from setuptools import setup
+
+setup(
+    name="pygerrit",
+    description="Client library for interacting with Gerrit",
+    version="0.0.2",
+    author="David Pursehouse",
+    author_email="david.pursehouse@sonymobile.com",
+    maintainer="David Pursehouse",
+    maintainer_email="david.pursehouse@sonymobile.com",
+    url="http://sonymobile.com",
+    packages=['pygerrit'],
+    keywords='gerrit, json',
+)
