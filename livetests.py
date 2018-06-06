693686eff0f6f6f210344f646a8f11296430d867
David Pursehouse <david.pursehouse@gmail.com>
2018-06-06 14:33:48 +0900

    Wrap livetests inside a class and separate to individual test methods
    Change-Id: I6839a929f261a0d4132bca0f11143e6f57dde920

--- livetests.py
+++ livetests.py
@@ -45,7 +45,7 @@ class GerritContainer(DockerContainer):
 
 
 @wait_container_is_ready()
-def _connect(api):
+def _initialize(api):
     api.get("/changes/")
 
 
@@ -60,46 +60,89 @@ def gerrit_api(request):
         else:
             auth = HTTPBasicAuth("admin", "secret")
         api = GerritRestAPI(url=url, auth=auth)
-        _connect(api)
+        _initialize(api)
         yield api
 
 
-def test_live_server(gerrit_api):
-    """Run the tests."""
-    # Create the project
-    projectinput = {"create_empty_commit": "true"}
-    gerrit_api.put("/projects/test-project", json=projectinput)
+class TestGerritAgainstLiveServer(object):
+    """Run tests against a live server."""
 
-    # Post with content as dict
-    changeinput = {"project": "test-project",
-                   "subject": "subject",
-                   "branch": "master",
-                   "topic": "topic"}
-    change = gerrit_api.post("/changes/", json=changeinput)
-    change_id = change["id"]
+    TEST_TOPIC = "test-topic"
 
-    # Get
-    gerrit_api.get("/changes/" + change_id)
+    def _get_test_change(self, gerrit_api):
+        results = gerrit_api.get("/changes/?q=topic:" + self.TEST_TOPIC)
+        assert len(results) == 1
+        return results[0]
 
-    # Put with content as string
-    gerrit_api.put("/changes/" + change_id + "/edit/foo",
-                   data="Content with non base64 valid chars åäö")
+    def test_put_with_json_dict(self, gerrit_api):
+        """Test a PUT request passing data as a dict to `json`.
 
-    # get content
-    response = gerrit_api.get("/changes/" + change_id + "/edit/foo",
-                              headers={'Accept': 'text/plain'})
+        Tests that the PUT request works as expected when data is passed
+        via the `json` argument as a `dict`.
 
-    # Will raise binascii.Error if content is not properly encoded
-    base64.b64decode(response)
+        Creates the test project which is used by subsequent tests.
 
-    # Put with no content
-    gerrit_api.put("/changes/" + change_id + "/edit/foo")
+        """
+        # Creates the project which will be used by subsequent tests
+        projectinput = {"create_empty_commit": "true"}
+        gerrit_api.put("/projects/test-project", json=projectinput)
+        gerrit_api.get("/projects/test-project")
 
-    # Review by API
-    rev = GerritReview()
-    rev.set_message("Review from live test")
-    rev.add_labels({"Code-Review": 1})
-    gerrit_api.review(change_id, "current", rev)
+    def test_post_with_json_dict(self, gerrit_api):
+        """Test a POST request passing data as a dict to `json`.
+
+        Tests that the POST request works as expected when data is passed
+        via the `json` argument as a `dict`.
+
+        Creates the change which is used by subsequent tests.
+        """
+        # Creates the change which will be used by subsequent tests
+        changeinput = {"project": "test-project",
+                       "subject": "subject",
+                       "branch": "master",
+                       "topic": self.TEST_TOPIC}
+        result = gerrit_api.post("/changes/", json=changeinput)
+        change = self._get_test_change(gerrit_api)
+        assert change["id"] == result["id"]
+
+    def test_put_with_data_as_string(self, gerrit_api):
+        """Test a PUT request passing data as a string to `data`.
+
+        Tests that the PUT request works as expected when data is passed
+        via the `data` parameter as a string.
+
+        Creates a change edit that is checked in the subsequent test.
+        """
+        change_id = self._get_test_change(gerrit_api)["id"]
+        gerrit_api.put("/changes/" + change_id + "/edit/foo",
+                       data="Content with non base64 valid chars åäö")
+
+    def test_get_base64_data(self, gerrit_api):
+        """Test a GET request on an API that returns base64 encoded response.
+
+        Tests that the headers can be overridden on the GET call, resulting
+        in the data being returned as text/plain, and that the content of the
+        response can be base64 decoded.
+        """
+        change_id = self._get_test_change(gerrit_api)["id"]
+        response = gerrit_api.get("/changes/" + change_id + "/edit/foo",
+                                  headers={'Accept': 'text/plain'})
+
+        # Will raise binascii.Error if content is not properly encoded
+        base64.b64decode(response)
+
+    def test_put_with_no_content(self, gerrit_api):
+        """Test a PUT request with no content."""
+        change_id = self._get_test_change(gerrit_api)["id"]
+        gerrit_api.put("/changes/" + change_id + "/edit/foo")
+
+    def test_review(self, gerrit_api):
+        """Test that a review can be posted by the review API."""
+        change_id = self._get_test_change(gerrit_api)["id"]
+        review = GerritReview()
+        review.set_message("Review from live test")
+        review.add_labels({"Code-Review": 1})
+        gerrit_api.review(change_id, "current", review)
 
 
 if __name__ == '__main__':
