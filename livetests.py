3f6af042bfb74a37197ab975b47f87bf5d0cb106
David Pursehouse <david.pursehouse@gmail.com>
2019-02-28 14:21:12 +0900

    Run livetests against 2.15.11 and 2.16.6
    Change-Id: I3624634e8e8f36f31d31a434399cb6d8affebf7a

--- livetests.py
+++ livetests.py
@@ -53,7 +53,7 @@ def _initialize(api):
 
 
 @pytest.fixture(scope="module",
-                params=["2.14.18", "2.15.10", "2.16.5"])
+                params=["2.14.18", "2.15.11", "2.16.6"])
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
     with GerritContainer(request.param) as gerrit:
