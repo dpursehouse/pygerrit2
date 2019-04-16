a2196867f7598e639246327d276afb124f2b6a8f
David Pursehouse <david.pursehouse@gmail.com>
2019-04-16 20:29:09 +0900

    Update livetest to use latest Gerrit releases
    Change-Id: I5c6481a302ca40ae8e1cb288306d7c461e36d587

--- livetests.py
+++ livetests.py
@@ -53,7 +53,7 @@ def _initialize(api):
 
 
 @pytest.fixture(scope="module",
-                params=["2.14.18", "2.15.11", "2.16.6"])
+                params=["2.14.19", "2.15.12", "2.16.7"])
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
     with GerritContainer(request.param) as gerrit:
