31f1c133a09d8117f87c79082bfd4c85b7ed090f
David Pursehouse <david.pursehouse@gmail.com>
2019-12-15 09:19:57 +0900

    Update live tests to latest versions
    - 2.16.15
    - 3.0.6
    - 3.1.2
    
    Change-Id: I64f6455cabd7781e32fbb2227078be25de64d4de

--- livetests.py
+++ livetests.py
@@ -52,7 +52,7 @@ def _initialize(api):
 
 
 @pytest.fixture(
-    scope="module", params=["2.14.20", "2.15.18", "2.16.14", "3.0.5", "3.1.1"]
+    scope="module", params=["2.14.20", "2.15.18", "2.16.15", "3.0.6", "3.1.2"]
 )
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
