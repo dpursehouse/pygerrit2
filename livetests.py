9191b817fab4439e2c3da8fc02dfb807c043bf63
David Pursehouse <david.pursehouse@gmail.com>
2020-06-04 10:54:25 +0900

    Add live tests against 3.2.0
    Change-Id: I3556ca96fafce1a43eb87eddda9abc6f47010d39

--- livetests.py
+++ livetests.py
@@ -52,7 +52,7 @@ def _initialize(api):
 
 
 @pytest.fixture(
-    scope="module", params=["2.16.20", "3.0.9", "3.1.5"]
+    scope="module", params=["2.16.20", "3.0.9", "3.1.5", "3.2.0"]
 )
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
