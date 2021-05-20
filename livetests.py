46d56cfc560ceefbd82b57a96f2d8a5473a1a849
Marco Miller <marco.miller@ericsson.com>
2021-05-20 14:18:14 -0400

    Add the newly released v3.4.0 to livetests images
    Remove the EOL ones now except the last 3.1 one; based on [1] as usual.
    
    Removing past EOL images also helps reduce livetests execution times and
    occasional CI flakiness.
    
    [1] https://www.gerritcodereview.com/support.html#supported-versions

--- livetests.py
+++ livetests.py
@@ -51,7 +51,7 @@ def _initialize(api):
 
 
 @pytest.fixture(
-    scope="module", params=["2.16.27", "3.0.16", "3.1.15", "3.2.10", "3.3.4"]
+    scope="module", params=["3.1.15", "3.2.10", "3.3.4", "3.4.0"]
 )
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
