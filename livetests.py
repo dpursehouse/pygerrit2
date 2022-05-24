31d53419f4f41214fa61d95185ea42ce40974db8
Marco Miller <marco.miller@ericsson.com>
2022-05-24 16:41:04 -0400

    Update livetests to use the latest v3.{4,5,6}

--- livetests.py
+++ livetests.py
@@ -50,7 +50,7 @@ def _initialize(api):
     api.get("/changes/")
 
 
-@pytest.fixture(scope="module", params=["3.4.4", "3.5.1"])
+@pytest.fixture(scope="module", params=["3.4.5", "3.5.2", "3.6.0"])
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
     with GerritContainer(request.param) as gerrit:
