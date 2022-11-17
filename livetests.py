97b373007c400bdac38a836a8bab21765b5a8f6d
Marco Miller <marco.miller@ericsson.com>
2022-11-17 13:38:41 -0500

    livetests: Bump to Gerrit v3.4.8, 3.5.4, and 3.6.3
    Add 3.7.0, alongside.
    
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>

--- livetests.py
+++ livetests.py
@@ -50,7 +50,7 @@ def _initialize(api):
     api.get("/changes/")
 
 
-@pytest.fixture(scope="module", params=["3.4.6", "3.5.3", "3.6.2"])
+@pytest.fixture(scope="module", params=["3.4.8", "3.5.4", "3.6.3", "3.7.0"])
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
     with GerritContainer(request.param) as gerrit:
