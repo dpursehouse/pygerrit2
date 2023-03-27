d50a0d25bb504990e104182d2ed25329ef673976
Marco Miller <marco.miller@ericsson.com>
2023-03-27 14:42:00 -0400

    Update livetests to use the current latest v3.7.2
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>

--- livetests.py
+++ livetests.py
@@ -50,7 +50,7 @@ def _initialize(api):
     api.get("/changes/")
 
 
-@pytest.fixture(scope="module", params=["3.4.8", "3.5.5", "3.6.4", "3.7.1"])
+@pytest.fixture(scope="module", params=["3.4.8", "3.5.5", "3.6.4", "3.7.2"])
 def gerrit_api(request):
     """Create a Gerrit container for the given version and return an API."""
     with GerritContainer(request.param) as gerrit:
